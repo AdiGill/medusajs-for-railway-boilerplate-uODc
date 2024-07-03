ef36c86c94e6a999cb379eebfc0bc7d5c3fc879b
Adi Gill <adisgill@gmail.com>
2024-07-03 04:01:19 -0400

    fix2.0?

--- medusajs-storefront/src/app/[countryCode]/(main)/comingsoon/page.tsx
+++ medusajs-storefront/src/app/[countryCode]/(main)/comingsoon/page.tsx
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import CountdownTimer from '@modules/countdown/countdown';
+// import React, { useState, useEffect } from 'react';
+// import CountdownTimer from '@modules/countdown/countdown';
 
-const CountdownPage: React.FC = () => {
-  const saleEndDate = '2024-07-20T00:00:00';
-  return (
-    <div className="flex flex-col items-center justify-center h-screen bg-stone-900 bg-[url('https://i.ibb.co/TMqH6Cy/Desktop-5.png')]">
-      <CountdownTimer targetDate={saleEndDate} />
+// const CountdownPage: React.FC = () => {
+//   const saleEndDate = '2024-07-20T00:00:00';
+//   return (
+//     <div className="flex flex-col items-center justify-center h-screen bg-stone-900 bg-[url('https://i.ibb.co/TMqH6Cy/Desktop-5.png')]">
+//       <CountdownTimer targetDate={saleEndDate} />
       
-    </div>
+//     </div>
     
-  );
-};
+//   );
+// };
 
-export default CountdownPage;
+// export default CountdownPage;
--- medusajs-storefront/src/modules/countdown/countdown.tsx
+++ medusajs-storefront/src/modules/countdown/countdown.tsx
@@ -1,68 +1,68 @@
-"use client";
+// "use client";
 
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
 
-interface CountdownTimerProps {
-  targetDate: string;
-}
+// interface CountdownTimerProps {
+//   targetDate: string;
+// }
 
-const CountdownTimer: React.FC<CountdownTimerProps> = ({ targetDate }) => {
-  const calculateTimeLeft = () => {
-    const difference = +new Date(targetDate) - +new Date();
-    let timeLeft = {};
+// const CountdownTimer: React.FC<CountdownTimerProps> = ({ targetDate }) => {
+//   const calculateTimeLeft = () => {
+//     const difference = +new Date(targetDate) - +new Date();
+//     let timeLeft = {};
 
-    if (difference > 0) {
-      timeLeft = {
-        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-        minutes: Math.floor((difference / 1000 / 60) % 60),
-        seconds: Math.floor((difference / 1000) % 60),
-      };
-    }
+//     if (difference > 0) {
+//       timeLeft = {
+//         days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+//         hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+//         minutes: Math.floor((difference / 1000 / 60) % 60),
+//         seconds: Math.floor((difference / 1000) % 60),
+//       };
+//     }
 
-    return timeLeft;
-  };
+//     return timeLeft;
+//   };
 
-  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+//   const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setTimeLeft(calculateTimeLeft());
-    }, 1000);
+//   useEffect(() => {
+//     const timer = setTimeout(() => {
+//       setTimeLeft(calculateTimeLeft());
+//     }, 1000);
 
-    return () => clearTimeout(timer);
-  }, [timeLeft]);
+//     return () => clearTimeout(timer);
+//   }, [timeLeft]);
 
-  const timerComponents: JSX.Element[] = [];
+//   const timerComponents: JSX.Element[] = [];
 
-  Object.keys(timeLeft).forEach((interval) => {
-    if ((timeLeft as any)[interval]) {
-      timerComponents.push(
-        <span key={interval} className="mx-1">
-          {(timeLeft as any)[interval]} {interval}{" "}
-        </span>
-      );
-    }
-  });
+//   Object.keys(timeLeft).forEach((interval) => {
+//     if ((timeLeft as any)[interval]) {
+//       timerComponents.push(
+//         <span key={interval} className="mx-1">
+//           {(timeLeft as any)[interval]} {interval}{" "}
+//         </span>
+//       );
+//     }
+//   });
 
-  return (
-    <div className="flex flex-col items-center justify-center p-4 bg-stonoe-900 rounded-lg">
-        <span className='text-4xl font-semi-bold tracking-widest text-white pb-8'>
-            01 - Inauguration
-        </span>
-      <div className="w-full border-t border-white"></div>
-      <div className="text-4xl font-medium text-white my-2 py-10 text-center">
-        {timerComponents.length ? timerComponents : <span>Time's up!</span>}
-      </div>
-      <div className="w-full border-t border-white"></div>
-      {/* <a
-          href="/store"
-          className=" animate-pulse  text-large-thin leading-10 tracking-widest text-white font-thin hover:border-b border-radius-10 hover:pb-0 pt-8 text-center justify-center" style={{ textShadow: "0 0 10px rgba(255, 255, 255, 0.8)" }} >
-            More Details→
+//   return (
+//     <div className="flex flex-col items-center justify-center p-4 bg-stonoe-900 rounded-lg">
+//         <span className='text-4xl font-semi-bold tracking-widest text-white pb-8'>
+//             01 - Inauguration
+//         </span>
+//       <div className="w-full border-t border-white"></div>
+//       <div className="text-4xl font-medium text-white my-2 py-10 text-center">
+//         {timerComponents.length ? timerComponents : <span>Time's up!</span>}
+//       </div>
+//       <div className="w-full border-t border-white"></div>
+//       {/* <a
+//           href="/store"
+//           className=" animate-pulse  text-large-thin leading-10 tracking-widest text-white font-thin hover:border-b border-radius-10 hover:pb-0 pt-8 text-center justify-center" style={{ textShadow: "0 0 10px rgba(255, 255, 255, 0.8)" }} >
+//             More Details→
           
-        </a> */}
-    </div>
-  );
-};
+//         </a> */}
+//     </div>
+//   );
+// };
 
-export default CountdownTimer;
+// export default CountdownTimer;
