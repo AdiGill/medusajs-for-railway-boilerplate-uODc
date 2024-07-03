ac486cbefb86cdbd23b6bc8d8fc3a434e1c3126f
Adi Gill <adisgill@gmail.com>
2024-07-03 17:28:54 -0400

    Erase - 0.0.2

--- medusajs-storefront/src/app/[countryCode]/(main)/comingsoon/page.tsx
+++ medusajs-storefront/src/app/[countryCode]/(main)/comingsoon/page.tsx
@@ -1,14 +1,11 @@
 import dynamic from 'next/dynamic';
 import React from 'react';
 
-// Dynamically import CountdownTimer without SSR
-const CountdownTimer = dynamic(() => import('@modules/countdown/countdown'), { ssr: false });
 
 const CountdownPage: React.FC = () => {
   const saleEndDate = '2024-07-20T00:00:00';
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-stone-900 bg-[url('https://i.ibb.co/TMqH6Cy/Desktop-5.png')]">
-      <CountdownTimer targetDate={saleEndDate} />
     </div>
   );
 };
