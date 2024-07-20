b201816720bba9efed2de38264ac6e1a397937dc
Adi Gill <adisgill@gmail.com>
2024-07-20 12:18:39 -0400

    b

--- medusajs-backend/medusa-config.js
+++ medusajs-backend/medusa-config.js
@@ -70,17 +70,14 @@ const plugins = [
         open: process.env.OPEN_BROWSER !== "false",
         port: ADMIN_APP_PORT,
       },
-    },
-  },
-  {
-    resolve: `medusa-payment-stripe`,
-    options: {
-      api_key: process.env.STRIPE_API_KEY,
-      webhook_secret: process.env.STRIPE_WEBHOOK_SECRET,
+      resolve: `medusa-payment-stripe`,
+        options: {
+          api_key: process.env.STRIPE_API_KEY,
+          webhook_secret: process.env.STRIPE_WEBHOOK_SECRET,
+        },
     },
   },
 ];
-
 
 const modules = {
   /*eventBus: {
